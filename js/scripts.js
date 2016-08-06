8019618d2ff2f0ae80e9385293d3b5e611a2b7f1
Sindhu <sbalakr2@ncsu.edu>
2016-08-06 17:58:03 -0400

    Committing website files

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+
+(function($) {
+    "use strict";
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+})(jQuery);
